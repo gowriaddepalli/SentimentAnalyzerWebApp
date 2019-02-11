0d93742a2684131b2aeed006d2c5795266b085a8
agd <gauriaddepalli@gmail.com>
2019-02-10 21:26:25 -0500

    Add files via upload

---
+++ SentimentAnalyzerClient/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import QueryForm from './components/query';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <QueryForm />
+      </div>
+    );
+  }
+}
+
+export default App;
